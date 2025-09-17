ae591f6d42cfe110adc06ba06001c6d268db4149
panks <paolo.panconi@fornitori.esselunga.it>
2025-09-17 10:18:57 +0200

    initial commit

---
+++ src/main/java/org/acme/article/Article.java
@@ -0,0 +1,13 @@
+package org.acme.article;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
