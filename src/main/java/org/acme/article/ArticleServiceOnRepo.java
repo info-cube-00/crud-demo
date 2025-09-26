4c1b36e139c88a1b394553951b1a03cf7c32b3cc
panks <paolo.panconi@fornitori.esselunga.it>
2025-09-26 16:44:07 +0200

    ..

--- src/main/java/org/acme/article/ArticleServiceOnRepo.java
+++ src/main/java/org/acme/article/ArticleServiceOnRepo.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
 public class ArticleServiceOnRepo implements ArticleService {
@@ -18,13 +20,13 @@ public class ArticleServiceOnRepo implements ArticleService {
     // TODO add needed annotations
     public ArticleVO create(ArticleVO articleVO) {
         //TODO create new article in database and return the created article as VO
-        throw new RuntimeException("Not yet implemented");
+        throw new WebApplicationException("Not implemented yet", Response.Status.NOT_IMPLEMENTED);
     }
 
     @Override
     public Optional<ArticleVO> load(Long id) {
         //TODO load article from database and return it as VO wrapped in an Optional
-        throw new RuntimeException("Not yet implemented");
+        throw new WebApplicationException("Not implemented yet", Response.Status.NOT_IMPLEMENTED);
     }
 
 
@@ -32,20 +34,20 @@ public class ArticleServiceOnRepo implements ArticleService {
     // TODO add needed annotations
     public ArticleVO update(ArticleVO articleVO) {
         //TODO update article in database and return the updated article as VO
-        throw new RuntimeException("Not yet implemented"); 
+        throw new WebApplicationException("Not implemented yet", Response.Status.NOT_IMPLEMENTED); 
     }
 
     @Override
     // TODO add needed annotations
     public void delete(ArticleVO articleVO) {
         //TODO ensure article is not present in database any longer
-        throw new RuntimeException("Not yet implemented"); 
+        throw new WebApplicationException("Not implemented yet", Response.Status.NOT_IMPLEMENTED); 
     }
 
     @Override
     public List<ArticleVO> listAll() {
         //TODO list all articles from database and return them as a list of VO
-        throw new RuntimeException("Not yet implemented"); 
+         throw new WebApplicationException("Not implemented yet", Response.Status.NOT_IMPLEMENTED); 
     }
 
 }
